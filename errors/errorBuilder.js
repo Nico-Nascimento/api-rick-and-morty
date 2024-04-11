3154c02ddb85ec9e10f213c28d1ff6422e9322a4
Nicolas Nascimento <nico.nascimento.97@gmail.com>
2024-04-11 18:47:45 -0300

    update

--- errors/errorBuilder.js
+++
@@ -1,9 +0,0 @@
-function createErrorContainer(message) {
-  container.innerHTML = `<div id="errorContainer">
-  <span>${message}</span>
-</div>`;
-}
-
-function renderError(message) {
-  createErrorContainer(message);
-}
